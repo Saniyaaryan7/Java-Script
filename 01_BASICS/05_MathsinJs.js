fb783ed357c3076c46423988c9e517e96556eb0a
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-12 13:19:19 +0000

    Maths in Js

---
+++ 01_BASICS/05_MathsinJs.js
@@ -0,0 +1,17 @@
+console.log(Math.abs(-4));   // Returns the absolute value → 4
+
+console.log(Math.round(4.6));  //  Rounds to the nearest integer → 5
+
+console.log(Math.ceil(6.8));     //  Rounds UP to the next integer → 7
+
+console.log(Math.floor(7.8));  //  Rounds DOWN to the previous integer → 7
+
+console.log(Math.min(5,7,8,9));
+
+console.log(Math.max(5,7,8,9));
+
+console.log(Math.random());    // Returns a random number between 0 (inclusive) and 1 (exclusive)
+
+console.log(Math.random()*10+1); // Returns a random number between 1 and 11 (not including 11)
+
+console.log(Math.floor(Math.random()*10)+1); //  Returns a random whole number between 1 and 10
