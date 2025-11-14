1e0958cf82ff0bdeaedf31c6d052f9a6716c1ab5
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-14 10:23:36 +0000

    Merge two arrays into one using concat()

---
+++ 02_BASICS/05_Concat.js
@@ -0,0 +1,6 @@
+const marvel_heros = ["Ironman","Thor","Spiderman"]
+const DC_heros = ["Flash","Superman","Batman"]
+
+const allHeros = marvel_heros.concat(DC_heros)  // Merge both arrays into a new single array 
+console.log(allHeros);
+console.log(allHeros[4]); //Superman
