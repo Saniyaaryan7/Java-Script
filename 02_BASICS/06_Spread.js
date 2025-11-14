771f6bb25d5d7ea411f07c60da5c559d6210dafb
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-14 10:30:40 +0000

    Merge two arrays using spread operator

---
+++ 02_BASICS/06_Spread.js
@@ -0,0 +1,6 @@
+const marvel_heros = ["Ironman","Thor","Spiderman"]
+const DC_heros = ["Flash","Superman","Batman"]
+
+const allHeros = [...marvel_heros,...DC_heros]  // Merge both arrays using spread operator (cleaner & modern)
+console.log(allHeros); 
+console.log(allHeros[5]); 
